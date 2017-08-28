b50169b93f16edc7a531774ef283b0a905f5a183
Quy <quy.nguyenngoctp@gmail.com>
2017-08-28 17:46:35 +0700

    Implement Chatting

---
+++ app/assets/javascripts/channels/chat.js
@@ -0,0 +1,18 @@
+function setupRoom(roomId) {
+  App.chat = App.cable.subscriptions.create(
+    {channel: "ChatChannel", id: roomId},
+    {
+      connected: function() {
+        // Called when the subscription is ready for use on the server
+      },
+
+      disconnected: function() {
+        // Called when the subscription has been terminated by the server
+      },
+
+      received: function(data) {
+        // Called when there's incoming data on the websocket for this channel
+      }
+    }
+  );
+}
